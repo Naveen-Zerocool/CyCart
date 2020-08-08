4f0ab8399f07dea39716396a56a92fafb1c58f27
Naveen Boobalakrishnan <ilovemybusiness.square@gmail.com>
2020-08-08 14:59:38 +0530

    When deleting a product, now doing soft delete

--- products/views.py
+++ products/views.py
@@ -7,3 +7,7 @@ from .models import Product
 class ProductsView(viewsets.ModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
+
+    def perform_destroy(self, instance):
+        instance.is_active = False
+        instance.save(update_fields=["is_active"])
