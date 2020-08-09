a4971bd0563dafcc3052bbff4e4030bb6753e1e1
Naveen Boobalakrishnan <ilovemybusiness.square@gmail.com>
2020-08-10 00:49:56 +0530

    Added docs

--- products/serializers.py
+++ products/serializers.py
@@ -1,3 +1,4 @@
+from drf_yasg.utils import swagger_serializer_method
 from rest_framework import serializers
 
 from products.models import Product, Cart, CartItem
@@ -10,11 +11,24 @@ class ProductSerializer(serializers.ModelSerializer):
         fields = "__all__"
 
 
+class CartItemSerializer(serializers.ModelSerializer):
+    title = serializers.CharField(source="product.title")
+    description = serializers.CharField(source="product.description")
+    price = serializers.DecimalField(source="product.price", max_digits=6, decimal_places=2)
+    image = serializers.URLField(source="product.image")
+
+    class Meta:
+        model = CartItem
+        fields = ["pk", "product", "title", "description", "price", "image", "quantity", "total_price", "discount",
+                  "final_price"]
+
+
 class CartSerializer(serializers.ModelSerializer):
     items = serializers.SerializerMethodField()
     final_price = serializers.SerializerMethodField()
 
     @staticmethod
+    @swagger_serializer_method(serializer_or_field=CartItemSerializer(many=True))
     def get_items(obj):
         return CartItemSerializer(CartItem.get_all_items_on_cart(obj), many=True).data
 
@@ -27,13 +41,23 @@ class CartSerializer(serializers.ModelSerializer):
         exclude = ["is_active", "user", "created_at", "modified_at"]
 
 
-class CartItemSerializer(serializers.ModelSerializer):
-    title = serializers.CharField(source="product.title")
-    description = serializers.CharField(source="product.description")
-    price = serializers.DecimalField(source="product.price", max_digits=6, decimal_places=2)
-    image = serializers.URLField(source="product.image")
+class CartAddSerializer(serializers.Serializer):
+    """
+    Serializer to represent in docs
+    """
+    product_id = serializers.IntegerField()
 
     class Meta:
-        model = CartItem
-        fields = ["pk", "product", "title", "description", "price", "image", "quantity", "total_price", "discount",
-                  "final_price"]
+        fields = ["product_id"]
+
+
+class CartEditSerializer(serializers.Serializer):
+    """
+    Serializer to represent in docs
+    """
+    product_id = serializers.IntegerField()
+    action = serializers.CharField(default="update/remove")
+    quantity = serializers.IntegerField()
+
+    class Meta:
+        fields = ["product_id", "action", "quantity"]
--- products/views.py
+++ products/views.py
@@ -1,11 +1,11 @@
+from drf_yasg.utils import swagger_auto_schema
 from rest_framework import viewsets
-from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
 from .cart import Cart
 from .constants import ACTION_UPDATE, ACTION_REMOVE
-from .serializers import ProductSerializer, CartSerializer
+from .serializers import ProductSerializer, CartSerializer, CartAddSerializer, CartEditSerializer
 from .models import Product
 
 
@@ -20,11 +20,19 @@ class ProductsView(viewsets.ModelViewSet):
 
 class CartViews(APIView):
 
+    @swagger_auto_schema(responses={200: CartSerializer})
     def get(self, request):
+        """
+        Get details of cart
+        """
         cart = Cart(request)
         return Response(CartSerializer(cart.cart).data)
 
+    @swagger_auto_schema(responses={201: CartSerializer}, request_body=CartAddSerializer)
     def post(self, request):
+        """
+        Add a product to Cart
+        """
         cart = Cart(request)
         product = None
         if request.data.get('product_id'):
@@ -33,7 +41,11 @@ class CartViews(APIView):
             cart.add_product(product)
         return Response(CartSerializer(cart.cart).data)
 
+    @swagger_auto_schema(responses={200: CartSerializer}, request_body=CartEditSerializer)
     def put(self, request):
+        """
+        Update or remove a product on Cart
+        """
         cart = Cart(request)
         product = None
         action_to_do = request.data.get('action')
