a2733b22c114bc517c90efa7588047529849887a
Naveen Boobalakrishnan <ilovemybusiness.square@gmail.com>
2020-08-10 13:15:40 +0530

    fixed a issue

--- products/urls.py
+++ products/urls.py
@@ -12,5 +12,5 @@ urlpatterns = [
     path('api/', include(router.urls)),
     url(r'^api/cart/$', CartViews.as_view(), name='user_cart'),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
-    url(r'', TemplateView.as_view(template_name='products/index.html'), name='purchase'),
+    url(r'^$', TemplateView.as_view(template_name='products/index.html'), name='purchase'),
 ]
